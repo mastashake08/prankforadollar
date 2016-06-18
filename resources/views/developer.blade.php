889d3c4d0a109afc81c93d473855bca59c65dd40
Jyrone Parker <jyrone.parker@gmail.com>
2016-06-18 05:00:58 +0000

    made images responsive

--- resources/views/developer.blade.php
+++ resources/views/developer.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">About Jyrone Parker</div>
 
                 <div class="panel-body">
-                    <img class="img img-rounded" src="https://en.gravatar.com/userimage/70717632/53adbdecac04d4ffbe3449993c901a73.jpeg"></img> <p>Jyrone Parker is an American software engineer and entrepreuner from Louisville, KY. When he is not spening time with his wife and 2 children or running his company <a href="http://jcompsolu.com">J Computer Solutions LLC</a>, he spends his time teaching people how to code for free <a href="http://www.jyroneparker.com">http://www.jyroneparker.com</a></p>
+                    <img class="img img-rounded img-responsive" src="https://en.gravatar.com/userimage/70717632/53adbdecac04d4ffbe3449993c901a73.jpeg"></img> <p>Jyrone Parker is an American software engineer and entrepreuner from Louisville, KY. When he is not spening time with his wife and 2 children or running his company <a href="http://jcompsolu.com">J Computer Solutions LLC</a>, he spends his time teaching people how to code for free <a href="http://www.jyroneparker.com">http://www.jyroneparker.com</a></p>
                 </div>
             </div>
         </div>
