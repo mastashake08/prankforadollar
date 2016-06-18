c73b5edabde247af6d226ddb63ae91f6f3bdbd8a
Jyrone Parker <jyrone.parker@gmail.com>
2016-06-18 08:28:23 +0000

    Structured data

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -8,12 +8,22 @@
                 <div class="panel-heading">Terms and Conditions</div>
 
                 <div class="panel-body">
-                    By using this application you agree not to:
-                    <ul>
-                      <li> Send hate mail </li>
-                      <li> Perform any illegal activites </li>
-                    </ul>
-                    If you perform any of the actions you are agreeing to idemify me Jyrone Parker from all criminal prosecution.
+
+“Application user shall fully indemnify, hold harmless and defend
+(collectively “indemnify” and “indemnification”) Jyrone Parker and his
+directors, officers, employees, agents, stockholders and Affiliates
+(collectively, “Indemnified Parties”) from and against all claims,
+demands, actions, suits, damages, liabilities, losses, settlements,
+judgments, costs and expenses (including but not limited to
+reasonable attorney’s fees and costs), whether or not involving a
+third party claim, which arise out of or relate to (1) any breach of
+any representation or warranty of Application user contained in this
+Agreement, (2) any breach or violation of any covenant or other
+obligation or duty of Application user under this Agreement or under
+applicable law, (3) (4) (5)] [other enumerated categories of claims
+and losses], in each case whether or not caused by the
+negligence of Jyrone Parker or any other Indemnified Party and whether or
+not the relevant Claim has merit.”
                 </div>
             </div>
         </div>
